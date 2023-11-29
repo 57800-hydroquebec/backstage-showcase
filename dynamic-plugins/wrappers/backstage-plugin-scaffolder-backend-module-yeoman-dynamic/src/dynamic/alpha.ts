c1a0aaf9233b4d2975fa3ff8e88431570e57d27c
Jason Froehlich <jfroehli@redhat.com>
2023-11-29 09:20:43 -0500

    Added yoeman plugin

---
+++ dynamic-plugins/wrappers/backstage-plugin-scaffolder-backend-module-yeoman-dynamic/src/dynamic/alpha.ts
@@ -0,0 +1,28 @@
+import { BackendDynamicPluginInstaller } from '@backstage/backend-plugin-manager';
+import {
+  coreServices,
+  createBackendModule,
+} from '@backstage/backend-plugin-api';
+import { scaffolderActionsExtensionPoint } from '@backstage/plugin-scaffolder-node/alpha';
+import { createRunYeomanAction } from '@backstage/plugin-scaffolder-backend-module-yeoman';
+
+export const dynamicPluginInstaller: BackendDynamicPluginInstaller = {
+  kind: 'new',
+  install: createBackendModule({
+    moduleId: 'scaffolder-backend-yeoman',
+    pluginId: 'scaffolder',
+    register(env) {
+      env.registerInit({
+        deps: {
+          scaffolder: scaffolderActionsExtensionPoint,
+          discovery: coreServices.discovery,
+        },
+        async init({ scaffolder }) {
+          for (const action of [createRunYeomanAction]) {
+            scaffolder.addActions(action({}));
+          }
+        },
+      });
+    },
+  }),
+};
